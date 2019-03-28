319263e00919bf93baccc94c825202dedaf6fcd8
cms638 <47014511+cms638@users.noreply.github.com>
2019-03-28 17:02:45 -0400

    Enemy Scripts

---
+++ Assets/__Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType {
+none,
+blaster,
+spread,
+phaser,
+missile,
+laser,
+shield
+}
+
+public class Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
